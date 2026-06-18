c0a1975f55bf97e822d1dd1929783d121b6c9058
Joey Smith (TML) <joey@joeysmith.com>
2026-06-18 03:11:45 -0600

    feat: wire async logger into daemon entry (M0 smoke path)

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,28 @@
 const std = @import("std");
+const logmod = @import("log/logger.zig");
+const Logger = logmod.Logger;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("conveyance 0.0.0\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const stderr = std.io.getStdErr();
+    const is_tty = std.posix.isatty(stderr.handle);
+
+    const log = try logmod.Log.init(allocator, 1024, .info, .{
+        .writer = stderr.writer().any(),
+        .format = if (is_tty) .pretty else .ndjson,
+        .color = is_tty,
+    });
+    defer {
+        log.deinit();
+        allocator.destroy(log);
+    }
+
+    const l = Logger.init(log, .session);
+    l.info("conveyance.start", &.{
+        .{ .key = "version", .value = .{ .str = "0.0.0" } },
+        .{ .key = "milestone", .value = .{ .str = "M0" } },
+    });
 }
